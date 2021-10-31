691556a8d2237ecd5c62f78af7cf30ffc099f694
Raj <rajsmarty2108@gmail.com>
2021-10-31 22:45:37 +0530

    bug fixes

--- advisorpedia/settings.py
+++ advisorpedia/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["djangoadvisor.herokuapp.com", "localhost"]
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangoadvisor.herokuapp.com", "localhost"]
 
 # Application definition
 INSTALLED_APPS = [
